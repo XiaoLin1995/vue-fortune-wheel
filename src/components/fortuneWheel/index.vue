72cf51771a983da46e9f79e917942a8ce73c552d
XiaoLin <xiaolin.zhu@justalk.com>
2020-12-25 10:34:51 +0800

    feature: v1.0.3 奖品文本将根据空格自动换行, 避免切断单词

--- src/components/fortuneWheel/index.vue
+++ src/components/fortuneWheel/index.vue
@@ -81,9 +81,9 @@ function getStrArray (str: string, len: number) {
     let text = str.substr(0, len)
     if (str.charAt(len) !== '' && str.charAt(len) !== ' ') { // 如果存在下一行并且下一行首字符不为空格
       const index = text.lastIndexOf(' ')
-      text = text.substr(0, index)
+      if (index !== -1) text = text.substr(0, index)
     }
-    str = str.replace(text, '')
+    str = str.replace(text, '').trim()
     arr.push(text)
   }
   return arr
